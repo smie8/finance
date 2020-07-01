6d36643b2c7073c31145f24703eeae29dd5f3396
Sami Eriksson <sami.eriksson@yahoo.fi>
2020-07-01 20:30:38 +0300

    Config removed from gitignore.

---
+++ config.py
@@ -0,0 +1,5 @@
+def getKey():
+    return 'G952y2FUocHOOkwKdLfXmLRRoELS'
+
+def getPostgresUri():
+    return 'postgres://oarxlexrxojpdg:28b0f1651cc8790a64949c6804b29242763b855372b782d344c387997e1645f4@ec2-35-173-94-156.compute-1.amazonaws.com:5432/d9l1e9pirshh41'
